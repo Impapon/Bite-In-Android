10268906c32ac8d475098a3547dc447ed814739e
papon sarker <paponsarker1@gmail.com>
2021-05-01 00:35:17 +0600

    add two activity commit

---
+++ app/src/main/java/com/example/sqlitebiteinkotlin/Spalish.kt
@@ -0,0 +1,11 @@
+package com.example.sqlitebiteinkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Spalish : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spalish)
+    }
+}
